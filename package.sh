5a68f77cd1d9218be8f3bfdbf98096789cc43c10
flatsiedatsie <unfold@gmail.com>
2019-08-12 14:03:26 +0000

    0.3.0

--- package.sh
+++ package.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-rm -f SHA256SUMS
-sha256sum -- package.json *.js LICENSE > SHA256SUMS
-npm pack
+# Download the latest version of the zigbee2mqtt project
+#if [ -d zigbee2mqtt ]
+#then
+#    rm -rf zigbee2mqtt
+#    echo "Removed old zigbee2mqtt project"
+#fi
 
+#echo "Downloading the latest version of zigbee2mqtt to sub directory"
+#git clone https://github.com/Koenkk/zigbee2mqtt.git
+
+# Install everything
+#echo "Getting all the dependencies for the add-on and for zigbee2mqtt"
+#npm install
+
+rm -f SHA256SUMS
+sha256sum -- package.json *.js build.sh LICENSE > SHA256SUMS
+
+echo "Packing..."
+npm pack
+echo "DONE"
